f445ae66f91eb4e86f618e9e1406e56ea8464828
Alexey Ovchinnikov <gdhald4@gmail.com>
2024-05-16 17:28:16 +0500

    11 task

--- 11Task/main.js
+++ 11Task/main.js
@@ -1,4 +1,13 @@
 let menuBtn = document.getElementById("menuBtn")
 function showPopUp(){
+    let PopUp = document.getElementsByClassName("PopUp")[0];
+    PopUp.style.display = "flex"; 
+}
+function closePopUp(){
+    let PopUp = document.getElementsByClassName("PopUp")[0];
+    PopUp.style.display = "none";
+}
+
+function AddPhoto(){
     
 }
