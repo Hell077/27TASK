ea3bfddbbf9396ff083f43c04216ff7fef5979cb
Ovchinnikov Alexey <gdhald4@gmail.com>
2024-05-17 20:51:23 +0500

    finnaly calcul

--- 12Task Calcul/main.js
+++ 12Task Calcul/main.js
@@ -20,6 +20,11 @@ function calculateResult() {
       throw new Error('Ошибка при делении на 0');
     }
 
+    // Обработка оператора квадратного корня перед другими операциями
+    currentString = currentString.replace(/√ (\d+(\.\d+)?)/g, (match, p1) => {
+      return Math.sqrt(parseFloat(p1));
+    });
+
     currentString = safeEval(currentString).toString();
   } catch (e) {
     currentString = 'Error: ' + e.message;
@@ -30,3 +35,28 @@ function calculateResult() {
 function safeEval(expression) {
   return new Function('return ' + expression)();
 }
+
+function Clear(){
+  currentString = "";
+  updateResult();
+}
+
+
+document.addEventListener('keydown', function(event) {
+  const key = event.key;
+  if (/\d/.test(key)) { 
+    appendNumber(key);
+  } else if (['+', '-', '*', '/'].includes(key)) { 
+    appendOperator(key);
+  } else if (key === 'Enter') { 
+    calculateResult();
+  } else if (key === 'Backspace') { 
+    currentString = currentString.slice(0, -1);
+    updateResult();
+  } else if (key === 'Escape') { 
+    clearString();
+  } else if (key === 'r' || key === 'R') { 
+    appendOperator('√');
+  }
+});
+
